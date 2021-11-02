0e5db3b093a16a25b543c0311262d40b3ecd78ed
Антипов Сергей <antip99999@mail.ru>
2021-11-02 14:17:37 +0300

    Создает шапку проекта

---
+++ source/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function() {
+  const burger = document.querySelector('.main-header__menu');
+  const headerMenu = document.querySelector('.main-header__list');
+  const userMenu = document.querySelector('.user-navigation');
+
+  burger.addEventListener('click', () => {
+    headerMenu.classList.toggle('active');
+    userMenu.classList.toggle('active');
+  })
+
+});
